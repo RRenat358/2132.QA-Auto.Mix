ca7a1f9dd81cceb6668af89232307fab5f452cb7
rrenat358 <rrenat358@gmail.com>
2023-10-22 14:19:09 +0300

    1419

---
+++ rest-api/reqres-in/src/test/java/DataDTO.java
@@ -0,0 +1,21 @@
+//import lombok.Getter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataDTO {
+
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+
+
+}
