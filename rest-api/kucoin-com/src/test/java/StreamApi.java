5b6777bf588594bcf539268d82491065e5c84270
rrenat358 <rrenat358@gmail.com>
2023-10-27 23:27:51 +0300

    2327

--- rest-api/kucoin-com/src/test/java/StreamApi.java
+++ rest-api/kucoin-com/src/test/java/StreamApi.java
@@ -2,15 +2,14 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
 public class StreamApi {
 
-    List<TickerData> getTickerDataList () {
+    List<TickerData> getTickerDataList() {
         return given()
                 .contentType(ContentType.JSON)
                 .when()
@@ -19,19 +18,19 @@ public class StreamApi {
                 .extract().jsonPath().getList("data.ticker", TickerData.class);
     }
 
+
     @Test
     public void checkCrypto() {
 
         List<TickerData> usdTickerDataList = getTickerDataList()
-                .stream().filter(ticker ->
-                        ticker.getSymbol().endsWith("USDT"))
+                .stream().filter(ticker -> ticker.getSymbol().endsWith("USDT"))
                 .collect(Collectors.toList());
-
     }
 
+
     @Test
-    public void sortHighToLow(){
-        List<TickerData> highToLow = getTickerDataList().stream().filter(x->x.getSymbol().endsWith("USDT")).sorted(new Comparator<TickerData>() {
+    public void sortHighToLow() {
+        List<TickerData> highToLow = getTickerDataList().stream().filter(x -> x.getSymbol().endsWith("USDT")).sorted(new Comparator<TickerData>() {
             @Override
             public int compare(TickerData o1, TickerData o2) {
                 return o2.getChangeRate().compareTo(o1.getChangeRate());
@@ -39,9 +38,36 @@ public class StreamApi {
         }).collect(Collectors.toList());
         List<TickerData> top10 = highToLow.stream().limit(10).collect(Collectors.toList());
 
-        Assertions.assertEquals(top10.get(0).getSymbol(),"BTC-USDT");
+        Assertions.assertEquals(top10.get(0).getSymbol(), "BTC-USDT");
     }
 
+
+    @Test
+    public void sortLowToHigh() {
+        List<TickerData> lowToHight = getTickerDataList()
+                .stream().filter(x -> x.getSymbol().endsWith("USDT"))
+                .sorted(new TickerComparatorLow()).limit(10)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void map() {
+
+        List<String> lowerCases = getTickerDataList()
+                .stream().map(x -> x.getSymbol().toLowerCase())
+                .collect(Collectors.toList());
+
+        //Вариант через map()
+        Map<String, Float> usd = new HashMap<>();
+        getTickerDataList()
+                .forEach(x -> usd.put(
+                        x.getSymbol(), Float.parseFloat(x.getChangeRate())));
+
+        //Вариант через отдельный класс DTO
+        List<TickerShort> shortList = new ArrayList<>();
+        getTickerDataList().forEach(x -> shortList.add(
+                new TickerShort(x.getSymbol(), Float.parseFloat(x.getChangeRate()))));
+    }
 
 
 
