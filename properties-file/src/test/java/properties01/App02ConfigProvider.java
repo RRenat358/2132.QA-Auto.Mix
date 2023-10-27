984cce35539cc17146f6daf1a6aaf9efba2df0fd
rrenat358 <rrenat358@gmail.com>
2023-10-27 20:51:57 +0300

    2051

--- properties-file/src/test/java/properties01/App02ConfigProvider.java
+++ properties-file/src/test/java/properties01/App02ConfigProvider.java
@@ -8,14 +8,20 @@ public interface App02ConfigProvider {
     Config config = readConfig();
 
     static Config readConfig(){
-        return ConfigFactory.systemProperties().hasPath("testProperties")
-                ? ConfigFactory.load(ConfigFactory.systemProperties().getString("testProperties"))
+        return ConfigFactory.systemProperties().hasPath("testProfile")
+                ? ConfigFactory.load(ConfigFactory.systemProperties().getString("testProfile"))
                 : ConfigFactory.load("app02.conf");
-
-
-
     }
 
+    String URL = readConfig().getString("url");
+    Integer AGE = readConfig().getInt("age");
+    String ADMIN_LOGIN = readConfig().getString("usersParams.admin.login");
+    String ADMIN_PASSWORD = readConfig().getString("usersParams.admin.password");
+    Boolean IS_ADMIN_ADMIN = readConfig().getBoolean("usersParams.admin.isAdmin");
+
+    String DEMO_PASSWORD = readConfig().getString("usersParams.demo.password");
+    String DEMO_LOGIN = readConfig().getString("usersParams.demo.login");
+    Boolean IS_DEMO_ADMIN = readConfig().getBoolean("usersParams.demo.isAdmin");
 
 
 
