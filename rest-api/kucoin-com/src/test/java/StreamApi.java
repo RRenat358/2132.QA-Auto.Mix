4918876b62e3a6d7b02f0fc8cdab72f11de28efd
rrenat358 <rrenat358@gmail.com>
2023-10-27 22:36:11 +0300

    2236

--- rest-api/kucoin-com/src/test/java/StreamApi.java
+++ rest-api/kucoin-com/src/test/java/StreamApi.java
@@ -1,17 +1,22 @@
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 public class StreamApi {
 
     @Test
     public void checkCrypto() {
-        given()
+        List<TickerData> tickerDataList = given()
                 .contentType(ContentType.JSON)
                 .when()
                 .get("https://api.kucoin.com/api/v1/market/allTickers")
-                .then().log().body();
+                .then().log().body()
+                .extract().jsonPath().getList("data.ticker", TickerData.class);
+
+
     }
 
 
