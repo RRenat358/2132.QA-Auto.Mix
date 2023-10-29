6feaa58964921dfac36bcddd131bb0c453b5e896
rrenat358 <rrenat358@gmail.com>
2023-10-29 22:50:53 +0300

    2250

--- ui-tests-selenide/hh-ru/src/test/java/HhruTest.java
+++ ui-tests-selenide/hh-ru/src/test/java/HhruTest.java
@@ -1,4 +1,5 @@
 import core.BaseSelenideTest;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,9 +20,9 @@ public class HhruTest extends BaseSelenideTest {
         //создаем карту ключ-значение с ожидаемыми данными
         Map<String,Object> expectedAttributes = new HashMap<>();
         expectedAttributes.put(ResumePage.GENDER,"М");
-        expectedAttributes.put(ResumePage.AGE, 25);
-        expectedAttributes.put(ResumePage.CITY, "Санкт-Петербург");
-        expectedAttributes.put(ResumePage.RELOCATE, false);
+        expectedAttributes.put(ResumePage.AGE, 41);
+        expectedAttributes.put(ResumePage.CITY, "Москва");
+        expectedAttributes.put(ResumePage.RELOCATE, true);
         expectedAttributes.put(ResumePage.CONFIRMED_PHONE, true);
 
         //получаем карту ключ-значение с актуальными данными
@@ -29,6 +30,31 @@ public class HhruTest extends BaseSelenideTest {
 
         //сравниваем ожидаемый и актуальный результат
         Assertions.assertEquals(expectedAttributes,actualAttributes);
+    }
+
+    @Test
+    public void checkAttributesClass(){
+        ResumePage resumePage = new ResumePage(URL_RESUME);
+        //создаем экземпляр класса с ожидаемыми данными через конструктор
+        ResumePage2 expectedResume = new ResumePage2("М", "Санкт-Петербург",25, true,false);
+
+        //получаем экземпляр класса с актуальными данными через конструктор
+        ResumePage2 actualResume = new ResumePage2(
+                resumePage.getGenderEasy(),
+                resumePage.getCityEasy(),
+                resumePage.getAge(),
+                resumePage.isPhoneConfirmed(),
+                resumePage.isReadyToRelocate());
+
+        //1 способ сравнивнения классов
+        Assertions.assertTrue(EqualsBuilder.reflectionEquals(expectedResume,actualResume));
+
+        //2 способ сравнения отдельных переменных в классе
+        Assertions.assertEquals(expectedResume.getGender(), actualResume.getGender());
+        Assertions.assertEquals(expectedResume.getCity(), actualResume.getCity());
+        Assertions.assertEquals(expectedResume.getAge(), actualResume.getAge());
+        Assertions.assertEquals(expectedResume.isNumberConfirmed(), actualResume.isNumberConfirmed());
+        Assertions.assertEquals(expectedResume.isReadyToRelocate(), actualResume.isReadyToRelocate());
     }
 
 
