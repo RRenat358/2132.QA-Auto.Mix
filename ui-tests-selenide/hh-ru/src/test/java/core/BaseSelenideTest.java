a55ad1d841869009669c62451024176e4df8b1ad
rrenat358 <rrenat358@gmail.com>
2023-10-30 16:44:25 +0300

    1644 темп

--- ui-tests-selenide/hh-ru/src/test/java/core/BaseSelenideTest.java
+++ ui-tests-selenide/hh-ru/src/test/java/core/BaseSelenideTest.java
@@ -2,7 +2,7 @@ package core;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -15,18 +15,19 @@ abstract public class BaseSelenideTest {
      * Инициализация selenide с настройками
      */
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.driverManagerEnabled = true;
+
+//        WebDriverManager.chromedriver().setup();
+//        Configuration.driverManagerEnabled = true;
+
 //        Configuration.webdriverLogsEnabled = true;
 
+        Configuration.browser = "chrome";
 //        Configuration.browserSize = "1280x720";  //HD. 720p
 //        Configuration.browserSize = "1366x768";  //FWXGA
 //        Configuration.browserSize = "1600x900";  //HD+
         Configuration.browserSize = "1920x1080"; // Full HD. 1080p
 //        Configuration.browserSize = "2560x1440"; //QHD. 1440p
 //        Configuration.browserSize = "1280x1280";   //user-custom
-
         Configuration.headless = false;
     }
 
