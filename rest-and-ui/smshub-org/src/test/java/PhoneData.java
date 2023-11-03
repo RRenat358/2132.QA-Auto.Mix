d052af8dac4b50cf6d78ff9da883197a64b98a90
rrenat358 <rrenat358@gmail.com>
2023-11-03 14:05:58 +0300

    1405

---
+++ rest-and-ui/smshub-org/src/test/java/PhoneData.java
@@ -0,0 +1,17 @@
+public class PhoneData {
+    private String numberId;
+    private String phoneNumber;
+
+    public PhoneData(String numberId, String phoneNumber) {
+        this.numberId = numberId;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNumberId() {
+        return numberId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
